4f98c721082409fddfbde7019a375eefbd700e0c
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-05-21 10:52:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmprestimoImobiliario
+ simula o valor das parcelas de um empréstimo imobiliário pelo Tabela SAC
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Emprestimo Imobiliario</title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
