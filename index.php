bbd3cf0482c2ab4a72a7eb71ef2777b11ac0c030
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-05-21 10:54:02 -0300

    Emprestimo imobiliario
    criação do projeto

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
         <title>Emprestimo Imobiliario</title>
     </head>
     <body>
+        
+        <h1> oi </h1>
+        <h2> oi </h2>
+        <h3> oi </h3>
+        <h4> oi </h4>
+        <h5> oi </h5>
+        <h6> oi </h6>
         <?php
         
         ?>
