db273388501fcef77de3c85c3c6b9d28d06820ba
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-05-22 09:26:47 -0300

    comit final
    codigo funcional

--- index.php
+++ index.php
@@ -1,18 +1,55 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Emprestimo Imobiliario</title>
+        <title>Simulação Financeira</title>
     </head>
     <body>
-        
-        <h1> oi </h1>
-        <h2> oi </h2>
-        <h3> oi </h3>
-        <h4> oi </h4>
-        <h5> oi </h5>
-        <h6> oi </h6>
+        <form method="POST">
+            <h2>Simulação de Financiamento pela Tabela SAC</h2>
+            Valor Financiado: <input type="text" name="valor"><br>
+            Número de Meses: <input type="number" name="meses" min="0" max="360" step="12"><br>
+            Taxa de Juros Mensal: <input type="number" name="taxa" min="0.1" max="20.0" step="0.2">
+            <input type="submit" name="enviar" value="Simular">
+        </form>
         <?php
-        
+            if(isset($_POST['enviar'])){
+                $valor_financiado = $_POST['valor'];
+                $prazo = $_POST['meses'];
+                $juros = $_POST['taxa'];
+                
+                $amortizacao = $valor_financiado/$prazo;
+                $saldo = $valor_financiado;
+                echo '<table>',
+                    '<tr>',
+                    '<td width = "50">'."Mês".'</td>',
+                    '<td width = "100">'."Amortização".'</td>',
+                    '<td width = "100">'."Parcela".'</td>',
+                    '<td width = "100">'."Saldo".'</td>',
+                    '</tr>',
+                    '</table>';
+                $cont = 1;
+                do {
+                    $juros_mes = $juros*$saldo/100;
+                    $parcela = $amortizacao+$juros_mes;
+                    $saldo -= $amortizacao;
+                    
+                    echo '<table>',
+                            '<tr>',
+                            '<td width = "50">'.$cont.'</td>',
+                            '<td width = "100">'. number_format($amortizacao,2,'.','').'</td>',
+                            '<td width = "100">'. number_format($parcela,2,'.','').'</td>',
+                            '<td width = "100">'. number_format($saldo,2,'.','').'</td>',
+                            '</tr>',
+                            '</table>';
+                    $cont++;
+                } while ($cont<=$prazo);
+            }
         ?>
     </body>
 </html>
